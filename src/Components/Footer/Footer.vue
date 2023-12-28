a5948d60a55a415e494ced84da479264413041e7
SathishWebie <sundarsathish05@gmail.com>
2023-12-29 01:20:08 +0530

    Logo fix

--- src/Components/Footer/Footer.vue
+++ src/Components/Footer/Footer.vue
@@ -28,7 +28,7 @@ const menus = [
 <template>
     <div class="flex px-3 lg:px-0 lg:items-center">
         <div class="w-6/12 lg:w-4/12">
-            <img src="../../Assets/footer-Logo.png" class="pt-3 lg:pt-0" />
+            <img src="../../Assets/footerlogo.png" class="pt-3 lg:pt-0" />
         </div>
         <div class="w-3/12 lg:w-8/12">
             <Menu :menu-list="menus" class="flex-col lg:flex-row lg:justify-end" />
